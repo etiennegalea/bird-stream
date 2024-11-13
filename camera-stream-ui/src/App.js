b950c408dddaeb3f16fd9ec5daa247636896b3c1
Etienne Galea <egalea11@gmail.com>
2024-11-13 23:21:21 +0100

    Updated camera stream display to use video tag instead of img

--- camera-stream-ui/src/App.js
+++ camera-stream-ui/src/App.js
@@ -6,11 +6,16 @@ function CameraStream() {
       <h1>Birb Stream</h1>
       <div style={{ border: '2px solid black', display: 'inline-block' }}>
         {/* Display the camera stream */}
-        <img
+        {/* <img
           src="http://localhost:8051/stream"
           alt="Birb Stream"
           style={{ width: '100%', height: 'auto' }}
-        />
+        /> */}
+        <video autoPlay muted loop>
+          <source src="http://localhost:8051/stream" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+
       </div>
     </div>
   );
