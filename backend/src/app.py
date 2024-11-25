917530b3f222638454b73d3b8e229072e57c92a6
Etienne Galea <egalea11@gmail.com>
2024-11-25 13:23:05 +0100

    moved connection_manager class to app.py temporarily

--- backend/src/app.py
+++ backend/src/app.py
@@ -8,7 +8,43 @@ from typing import List
 from time import time
 import logging
 
-from connection_manager import ConnectionManager
+# from connection_manager import ConnectionManager
+
+from fastapi import WebSocket, WebSocketDisconnect
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.StreamHandler()
+    ],
+)
+
+logger = logging.getLogger("connection_manager")
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        logger.error(f"active_connections (connect) -- {self.active_connections}")
+        self.active_connections.append(websocket)
+
+    async def disconnect(self, websocket: WebSocket):
+        logger.error(f"active_connections (disconnect) -- {self.active_connections}")
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+    def count_connections(self):
+        return len(self.active_connections)
 
 logging.basicConfig(
     level=logging.INFO,
